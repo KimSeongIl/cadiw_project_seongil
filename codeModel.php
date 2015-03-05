bbfb05ea907faa1090d81104efdc6308e73d3026
KimSeongIl <ksi4687@nate.com>
2015-03-05 20:12:01 +0900

    게시판 검색 추가

--- codeModel.php
+++ codeModel.php
@@ -47,7 +47,38 @@ class CodeModel extends CI_Model{
 		$this->db->where('bid',$no);
 		$this->db->delete('board');
 	}
-	
+	function boardSearchAll($num,$local,$search){
+		$this->db->limit($num,$local);
+		$this->db->select('bid,btitle,name,bdate');
+		$this->db->from('board');
+		$this->db->join('member','member.id=board.uid');
+		$this->db->like('btitle',$search);
+		$this->db->or_like('bcontent',$search);
+		$this->db->order_by("bid", "desc"); 
+		return $this->db->get()->result();
+	}
+	function boardSearchTitle($num,$local,$search){
+		$this->db->limit($num,$local);
+		$this->db->select('bid,btitle,name,bdate');
+		$this->db->from('board');
+		$this->db->join('member','member.id=board.uid');
+		$this->db->like('name',$search);
+		$this->db->order_by("bid", "desc"); 
+		return $this->db->get()->result();
+	}
+	function boardSearchAllCount($search){
+		$this->db->from('board');
+		$this->db->join('member','member.id=board.uid');
+		$this->db->like('btitle',$search);
+		$this->db->or_like('bcontent',$search);
+		return $this->db->count_all_results();
+	}
+	function boardSearchTitleCount($search){
+		$this->db->from('board');
+		$this->db->join('member','member.id=board.uid');
+		$this->db->like('name',$search);
+		return $this->db->count_all_results();
+	}
 	
 }
 ?>
