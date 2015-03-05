c80c190e8b9eda371d7c073df356d90237cf199d
KimSeongIl <ksi4687@nate.com>
2015-03-05 18:36:09 +0900

    게시판 수정 삭제 추가

--- boardView.php
+++ boardView.php
@@ -1,3 +1,7 @@
+<?
+	$udata=$this->session->all_userdata();
+	
+?>
 <div id="article">
 <center>
 	<table border id="boardView">
@@ -22,6 +26,16 @@
 	<br>
 	<input type="button" value="돌아가기" class="btn btn-default btn-sm" onclick="history.back();">
 </center>
+<? if($udata['uid']==$list[0]->uid){?>
+<div id='boardControl' align="right">
+	<form action="/index.php/cpms/code/boardDelete" method="post">
+	<input type="button" value="수정" class="btn btn-default btn-sm" onclick="location.href='/index.php/cpms/code/boardUpdate/<?=$list[0]->bid?>'"></a>
+	&nbsp;&nbsp;
+	<input type="submit" value="삭제" class="btn btn-default btn-sm" onclick="if(!confirm('정말 삭제하시겠습니까?')){return false;}"><br>
+	<input type="hidden" value="<?=$list[0]->bid?>" name="bid">
+	</form>
+</div>
+<?}?>
 </div><!-- div id='article'-->
 </div> <!--div id='wrap'-->
 
